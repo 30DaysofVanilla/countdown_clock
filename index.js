d780d46efa9050a2db937d1fa76e1ce93e95f44a
Shawn Southwell <shawn.southwel@gmail.com>
2016-12-12 15:32:40 -0800

    convert time input to json

--- index.js
+++ index.js
@@ -3,33 +3,44 @@ document.addEventListener('DOMContentLoaded', () => {
   const stopButton = document.getElementById('stopTimer');
   const timer = document.getElementById('timer');
   const form = document.getElementById('timerForm');
-  const input = document.getElementById('timerInput')
-
+  const input = document.getElementById('timerInput');
   let timerDuration;
 
-  function startTimer(duration){
+  function startTimer(){
     window.timerInterval = setInterval(()=>{
-      console.log('fired')
-      duration -= 1;
-      timer.innerHTML = duration},1000)
+      if (timerDuration !== 0){
+        timerDuration -= 1;
+        timer.innerHTML = timerDuration
+      } else {
+        stopTimer(window.timerInterval);
+      }
+    },1000)
   }
 
   function stopTimer(interval){
-    console.log(interval)
-    window.clearInterval(interval);
-    return;
+    return window.clearInterval(interval);
   }
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const val = +input.value;
-    console.log(val);
-  })
+  //convert 1, 2, .15 ---> 01:00:00, 02:00:00, 00:15:00,
+  function convertTime(time){
+    time = time.includes('.') ? `00${time}` : time;
+    const [hour = '00', min = '00'] = time.split('.');
+    return { hour, min, sec: '00' };
+  }
+
   button.addEventListener('click', (e) => {
-    console.log('button clicked');
-    startTimer(15)
+    startTimer();
   })
 
   stopButton.addEventListener('click', (e) => {
     stopTimer(window.timerInterval);
   })
+
+  document.getElementsByClassName('navbar')[0].addEventListener('click', (e) => {
+    if (!e.srcElement.getAttribute('data-time')) return;
+    const duration = e.srcElement.getAttribute('data-time');
+    const time = duration.includes('m') ?
+    `.${duration.replace(/[^0-9]/g,'')}` : `${duration.replace(/[^0-9]/g,'')}`;
+    console.log('convertTime evaluates to', convertTime(time));
+    })
+
 });
